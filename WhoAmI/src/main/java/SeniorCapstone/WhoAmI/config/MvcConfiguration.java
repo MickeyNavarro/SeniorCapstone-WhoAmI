ccb97a79441b432a4c11c6ea1e54141a27a25bd4
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2021-02-07 21:14:18 -0700

    whoops i uploaded everything to the wrong git - here is the right one

---
+++ WhoAmI/src/main/java/SeniorCapstone/WhoAmI/config/MvcConfiguration.java
@@ -0,0 +1,33 @@
+package SeniorCapstone.WhoAmI.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages="SeniorCapstone.WhoAmI")
+@EnableWebMvc
+@ImportResource(value = {"classpath:/SeniorCapstone/WhoAmI/config/applicationConfiguration.xml"})
+public class MvcConfiguration extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public ViewResolver getViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	
+}
